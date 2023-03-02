5fee3837925a3cfff7172fd5e59278e7405b2814
JJ Shea <jjshea@live.com>
2023-03-02 23:06:21 +0700

    Add color sketch functionality and ability to resize sketch board
    Modified javascript code to allow for various colors to be added
    during mouseover events across the etch-a-sketch board. Also added
    the ability for user to resize the number of board rows/columns to
    provide higher/lower levels of granularity, limited from 2-100 rows
    or columns. Lastly, added ability to switch between different pen
    colors, including a rainbow option, and white color as an "eraser".

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
-let size = 16;
-let area = size * size;
-let board = document.querySelector(".board");
+let color = "black";
 
 function populateBoard(size) {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove);
     board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
     board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
-    
+
+    let area = size * size;
+
     for (let i = 0; i<area; i++) {
         let square = document.createElement("div");
-        square.style.backgroundColor = "lightgrey";
+        square.style.backgroundColor = "white";
         board.insertAdjacentElement("beforeend", square); 
-        square.addEventListener("mouseover", (event) => {event.target.style.backgroundColor = "red"});
+        square.addEventListener("mouseover", colorSquare);
     };
 };
 
-populateBoard(size);
+populateBoard(16);
+
+function newSize(input) {
+    if (input >= 2 && input <= 100) {
+        populateBoard(input);
+    }
+    else {
+        alert("Please enter a number between 2 and 100");
+    };
+};
+
+function colorSquare() {
+    if (color === 'rainbow') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    }
+    else {
+        this.style.backgroundColor = color;
+    };
+};
+
+function changeColor(choice) {
+    color = choice;
+}
