92cfb1271e5638bfe0293ebc4f05c0e47555ccdc
JJ Shea <jjshea@live.com>
2023-03-02 17:01:54 +0700

    Create hover effect on mouseover event
    Used javascript to add a mouseover event to each of the squares
    on my etch-a-sketch grid which turns them red when the mouse passes
    over them. No timeout on the color change, at this point, so the
    red color is permanent after a single pass.

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 let size = 16;
 let area = size * size;
+let board = document.querySelector(".board");
 
 function populateBoard(size) {
-    let board = document.querySelector(".board");
     board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
     board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
     
     for (let i = 0; i<area; i++) {
-        let square = document.createElement('div');
-        square.style.backgroundColor = 'lightgrey';
-        board.insertAdjacentElement('beforeend', square);
+        let square = document.createElement("div");
+        square.style.backgroundColor = "lightgrey";
+        board.insertAdjacentElement("beforeend", square); 
+        square.addEventListener("mouseover", (event) => {event.target.style.backgroundColor = "red"});
     };
 };
 
 populateBoard(size);
-
-
-
