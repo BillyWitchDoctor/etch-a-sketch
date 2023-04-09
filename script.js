600ab93a405f8f41f938069b064c75bd9ee125cf
JJ Shea <jjshea@live.com>
2023-04-09 13:40:57 +1200

    Fix clear button function and resize input function
    The clear button now correctly clears all colors from the board
    and when the user inputs a new board size, colors will also be
    cleared from board.
    
    Adds pencil button option but have not yet been able to
    implement the 10% incremental 'shading' ability from extra credit
    section of TOP for this exercise.
    
    Removed initial prompt for user to choose board size, for now.
    Defaults back to 16x16 initial board size, however the input
    field now correctly clears out text/number when user hits enter
    or clicks on Change Board Size button.

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function populateBoard(size) {
 
     let area = size * size;
 
+
     for (let i = 0; i<area; i++) {
         let square = document.createElement("div");
         square.style.backgroundColor = "white";
@@ -22,10 +23,17 @@ populateBoard(16);
 function newSize(input) {
     if (input >= 2 && input <= 100) {
         populateBoard(input);
+
+        let boardSizeInput = document.getElementById('board_size');
+        boardSizeInput.value = '';
+
+        let board = document.querySelector(".board");
+        let squares = board.querySelectorAll("div");
+        squares.forEach((div) => div.style.backgroundColor = "white");
     }
     else {
         alert("Please enter a number between 2 and 100");
-    };
+    }
 };
 
 function colorSquare() {
@@ -39,4 +47,10 @@ function colorSquare() {
 
 function changeColor(choice) {
     color = choice;
-}
+};
+
+function resetBoard() {
+    let board = document.querySelector(".board");
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "white");
+};
